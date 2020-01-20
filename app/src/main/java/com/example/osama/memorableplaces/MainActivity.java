313d68f92f9c0cdd6c96a34a33fb5d801bc86254
Osama Saleh <osama.saleh@mail.utoronto.ca>
2020-01-19 22:46:13 -0500

    Memoranda application

---
+++ app/src/main/java/com/example/osama/memorableplaces/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.osama.memorableplaces;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    //Accessiible from any activity
+    static ArrayList<String> places = new ArrayList<>();
+    static ArrayList<LatLng> locations = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.placesView);
+
+        //Store places and locations
+
+
+        places.add("Add a new place");
+        locations.add(new LatLng(0,0));
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("placeNumber", position);
+
+                startActivity(intent);
+
+
+
+            }
+        });
+    }
+
+}
